a272e0988f4683970bdcd7a992adfb43db791b13
dbp <byronpantoja@gmail.com>
2022-02-26 06:26:36 +0800

    adding jsconfig and sitemetadata and SEO files

---
+++ components/SEO.js
@@ -0,0 +1,8 @@
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import siteMetadata from '@/data/siteMetadata'
+
+const CommonSEO = ({ title, description, }) => {
+  const router = useRouter()
+  return ()
+}
