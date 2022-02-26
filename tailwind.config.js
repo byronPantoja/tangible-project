abd5541864c130bd7f9b0e5004ae3385588f6d48
dbp <byronpantoja@gmail.com>
2022-02-26 11:33:41 +0800

    adding free trial pages and form

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       padding: { 'fluid-video': '56.25%' },
