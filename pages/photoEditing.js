3baf1f5be2ab859147bf0ba9e147ff9da08401ce
dbp <byronpantoja@gmail.com>
2022-02-26 09:19:56 +0800

    adding pages for services

---
+++ pages/photoEditing.js
@@ -0,0 +1,10 @@
+import Navbar from '@/components/Navbar'
+
+export default function PhotoEditing() {
+  return (
+    <div>
+      <Navbar />
+      Photo Editing
+    </div>
+  )
+}
---
+++ pages/videoEditing.js
@@ -0,0 +1,10 @@
+import Navbar from '@/components/Navbar'
+
+export default function videoEditing() {
+  return (
+    <div>
+      <Navbar />
+      Video Editing
+    </div>
+  )
+}
