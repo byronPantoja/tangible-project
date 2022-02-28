2d634be2d5ae648750134522a73a9d650a7f0415
dbp <byronpantoja@gmail.com>
2022-03-01 03:09:25 +0800

    adding samples page and fixing links in companyData.js

--- data/companyData.js
+++ data/companyData.js
@@ -1,8 +1,8 @@
 import { BriefcaseIcon, InformationCircleIcon, OfficeBuildingIcon } from '@heroicons/react/outline'
 
 const company = [
-  { name: 'About', href: '#', icon: InformationCircleIcon },
-  { name: 'Contact', href: '#', icon: OfficeBuildingIcon },
-  { name: 'FAQ', href: '#', icon: BriefcaseIcon },
+  { name: 'About', href: '/about', icon: InformationCircleIcon },
+  { name: 'Contact', href: '/contact', icon: OfficeBuildingIcon },
+  { name: 'FAQ', href: '/faq', icon: BriefcaseIcon },
 ]
 module.exports = company
---
+++ pages/faq.js
@@ -0,0 +1,10 @@
+import Navbar from '@/components/Navbar'
+
+export default function Faq() {
+  return (
+    <div>
+      <Navbar />
+      FAQ
+    </div>
+  )
+}
