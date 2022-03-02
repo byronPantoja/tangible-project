e67f6c193cb4f4f203ffedc3927ecd619ad2a4fe
dbp <byronpantoja@gmail.com>
2022-03-02 15:49:21 +0800

    adding Image comparison component slider

--- pages/photoEditing.js
+++ pages/photoEditing.js
@@ -1,27 +1,93 @@
-import { useState } from 'react'
-import Image from '@/components/Image'
+import ComparisonSlider from '@/components/ComparisonSlider'
+import Navbar from '@/components/Navbar'
+
+import {
+  CloudUploadIcon,
+  CogIcon,
+  LockClosedIcon,
+  RefreshIcon,
+  ServerIcon,
+  ShieldCheckIcon,
+} from '@heroicons/react/outline'
+
+const features = [
+  {
+    name: 'Push to Deploy',
+    description:
+      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: CloudUploadIcon,
+  },
+  {
+    name: 'SSL Certificates',
+    description:
+      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Simple Queues',
+    description:
+      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: RefreshIcon,
+  },
+  {
+    name: 'Advanced Security',
+    description:
+      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: ShieldCheckIcon,
+  },
+  {
+    name: 'Powerful API',
+    description:
+      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: CogIcon,
+  },
+  {
+    name: 'Database Backups',
+    description:
+      'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: ServerIcon,
+  },
+]
 
 const photoEditing = () => {
-  const [imageRevealFraq, setImageRevealFraq] = useState(0.5)
   return (
-    <div className="px-4">
-      <div className="relative mx-auto mt-32 w-full max-w-lg">
-        <img
-          src="https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62084f307eb94a2d215ecf41/1644711729229/Done-1.jpg"
-          alt="before"
-        />
-
-        <img
-          style={{
-            filter: 'grayscale(100%)',
-            clipPath: `polygon(0 0, ${imageRevealFraq * 100}% 0, ${
-              imageRevealFraq * 100
-            }% 100%, 0 100%)`,
-          }}
-          src="https://static1.squarespace.com/static/60be3f15c04ff53428559e91/t/62084f386f98284656405b54/1644711737168/Done-2.jpg"
-          alt="after"
-          className="absolute inset-0"
-        />
+    <div>
+      <Navbar />
+      <ComparisonSlider />
+      <div className="relative bg-white py-16 sm:py-24 lg:py-32">
+        <div className="mx-auto max-w-md px-4 text-center sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="text-base font-semibold uppercase tracking-wider text-indigo-600">
+            Deploy faster
+          </h2>
+          <p className="mt-2 text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Everything you need to deploy your app
+          </p>
+          <p className="mx-auto mt-5 max-w-prose text-xl text-gray-500">
+            Phasellus lorem quam molestie id quisque diam aenean nulla in. Accumsan in quis quis
+            nunc, ullamcorper malesuada. Eleifend condimentum id viverra nulla.
+          </p>
+          <div className="mt-12">
+            <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+              {features.map((feature) => (
+                <div key={feature.name} className="pt-6">
+                  <div className="flow-root rounded-lg bg-gray-50 px-6 pb-8">
+                    <div className="-mt-6">
+                      <div>
+                        <span className="inline-flex items-center justify-center rounded-md bg-indigo-500 p-3 shadow-lg">
+                          <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+                        </span>
+                      </div>
+                      <h3 className="mt-8 text-lg font-medium tracking-tight text-gray-900">
+                        {feature.name}
+                      </h3>
+                      <p className="mt-5 text-base text-gray-500">{feature.description}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
