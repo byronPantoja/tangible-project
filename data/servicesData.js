5b35d75e573e3d1a1c9a395d200260c4f5c54287
dbp <byronpantoja@gmail.com>
2022-03-03 15:32:44 +0800

    adding more writing to photo-services |  changed files names of services to x-services.js

--- data/servicesData.js
+++ data/servicesData.js
@@ -4,19 +4,19 @@ const services = [
   {
     name: 'Photo Editing',
     description: 'Real Estate Photo Editing / Processing - $0.50 each',
-    href: '/photoEditing',
+    href: '/photo-services',
     icon: ChartBarIcon,
   },
   {
     name: 'Video Editing',
     description: 'Video Editing - Starting at $5',
-    href: '/videoEditing',
+    href: '/video-services',
     icon: CursorClickIcon,
   },
   {
     name: 'Twilight',
     description: 'Virtual Twilight - $5 Each',
-    href: '/twilightEditing',
+    href: '/twilight-services',
     icon: ViewGridIcon,
   },
 ]
