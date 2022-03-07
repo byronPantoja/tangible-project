58472f92fa88af6aa2fcb5bc6dbe8d286a845733
dbp <byronpantoja@gmail.com>
2022-03-07 17:59:24 +0800

    deleting old Comparison component

--- components/ComparisonSlider.js
+++
@@ -1,8 +0,0 @@
-import CompareImagesSlider from './CompareImagesSlider'
-
-const ComparisonSlider = (input1, input2) => {
-  
-  return {theSlider(beforeImage, afterImage)}
-}
-
-export default ComparisonSlider
